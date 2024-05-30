34515302e1818200f9e729521d52bfc1489ff4b5
wessam18 <wessamabdelnabi903@gmail.com>
2024-05-30 04:15:03 +0300

    1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+    new Promise((resolve, reject) => {
+        if (success) {
+            resolve({
+                status: 200,
+                body: 'Success'
+            });
+        } else {
+            reject (new Error("The fake API is not working currently"));
+        }
+    })
+}
