86ac4d92c5fa933961156bb811b7a48a72c0c128
wessam18 <wessamabdelnabi903@gmail.com>
2024-06-05 03:30:27 +0300

    6

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,7 @@
+export default function updateStudentGradeByCity(students, city, newgrades) {
+    return students
+    .filter((students) => students.location === city)
+    .map(function(students, newgrades) {
+
+     })
+}
