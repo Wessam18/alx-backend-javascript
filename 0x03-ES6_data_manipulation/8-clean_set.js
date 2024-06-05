f45e8ae476a97a9722b6aa78460f2c6af05c84a7
wessam18 <wessamabdelnabi903@gmail.com>
2024-06-05 04:05:30 +0300

    .

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-    const cleanedValues = [];
-    for (const value of set) {
-        if (value.indexOf(startString) === 0) {
-            cleanedValues.push(value.substring(startString.length));
-        }
+  const cleanedValues = [];
+  for (const value of set) {
+    if (value.indexOf(startString) === 0) {
+      cleanedValues.push(value.substring(startString.length));
     }
-    return cleanedValues.join("-");
+  }
+  return cleanedValues.join('-');
 }
