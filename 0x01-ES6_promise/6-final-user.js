5d55e420e66c4e9f02730a50d24b32bdfe81b71c
wessam18 <wessamabdelnabi903@gmail.com>
2024-06-09 17:15:09 +0300

    .

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, photo) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(photo)])
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map(({ status, value, reason }) => ({
       status,
-      value: status === 'fulfilled' ? value : reason.toString(),
+      value: status === 'fulfilled' ? value : reason,
     })));
 }
