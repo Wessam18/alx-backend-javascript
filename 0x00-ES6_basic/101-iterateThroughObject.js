2022b397a5720024ca60c66ac4a9ee9d81bdcbc6
wessam18 <wessamabdelnabi903@gmail.com>
2024-06-21 02:43:44 +0300

    101

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+    const employees = [];
+    for (const idx of reportWithIterator) {
+      employees.push(idx);
+    }
+    return employees.join(' | ');
+  }
